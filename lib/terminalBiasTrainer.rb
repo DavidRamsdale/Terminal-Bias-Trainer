93ad410794e3257fc1b7d3aaae7aa868995baa43
Adam Demirel <adxm@msn.com>
2018-09-04 17:01:17 +1000

    Add leaveback form.
    Co-authored-by: David Ramsdale <davidramsdale1991@gmail.com>

--- lib/terminalBiasTrainer.rb
+++ lib/terminalBiasTrainer.rb
@@ -6,7 +6,7 @@
 require_relative 'questionsAnswers.rb'
 #require_relative 'countdownTest.rb'
 require_relative 'user.rb'
-@array = [Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8, Q9, Q10]
+array = [Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8, Q9, Q10]
 hash = {Q1 => [A1, A2], Q2 => [B1, B2], Q3 => [C1, C2], Q4 => [D1, D2], Q5 => [E1, E2], Q6 => [F1, F2], Q7 => [G1, G2], Q8 => [H1, H2], Q9 => [I1, I2], Q10 => [J1, J2]}
 
 #These are our variables
@@ -50,5 +50,16 @@ while i < 4 do
 
     # Delete question from array
     @array.delete_at(position)
+
+    puts "Would you like to leave feedback? Type <yes> or <no>."
+    feedback = gets.chomp
+    if feedback == 'yes' then
+        puts "Sure, what would you like to say?"
+        yes_input = gets.chomp
+        open('feedback.rb', 'a') { |f| f.puts "Feedback: '#{yes_input}'\n\n" }
+    else
+        puts "That's okay."
+    end
+        puts "Thanks for playing :)"
 end
 
