7e28a7faa39ce2a33d240f8e5636e0250b0044e6
Adam Demirel <adxm@msn.com>
2018-09-04 14:27:18 +1000

    Add sample questions and answers.

---
+++ lib/questionsAnswers.rb
@@ -0,0 +1,13 @@
+Q1 = "For a period of 1 year, each hospital recorded the days on which more than 60 percent of the babies born were boys. Which hospital do you think recorded more such days?\na) The larger hospital\nb) The smaller hospital\nc) About the same (that is, within 5 percent of each other) (type a, b, or c)"
+A1 = 'c'
+A2 = "The correct answer is b) The smaller hospital. You likely scored wrong because you are comitting a mental shortcut called the representativeness heuristic. This means that you quickly and easily assume that bigger things like hospitals have larger quantities like male babies. Although the larger hospital has more male babies in total, the question is asking about the percentage of male babies. The bias we are using and the error we are committing is called insensitivity to sample size, which means that we do not realise that as the size of a study population get’s bigger, the less likely deviating from the average becomes. In reality, the larger the hospital, the less likely it is that the percentage of babies being males will deviate from being 50\%. And with the sample size large enough, the percentage of male babies will recede to be very near 50%."
+
+Q2 = 1
+B1 = 1
+B2 = 1
+Q3 = 2
+C1 = 2
+C2 = 2
+Q4 = 3
+D1 = 3
+D2 = 3
--- lib/terminalBiasTrainer.rb
+++ lib/terminalBiasTrainer.rb
@@ -1,14 +1,17 @@
-puts "Welcome to our app"
-sleep(2) #to be prolonged, make it 45 seconds
+#puts "Welcome to our app"
+#sleep(2) #to be prolonged, make it 45 seconds
 
 #Dry this out
-array = [:Q1 , :Q2, :Q3, :Q4]
-hash = {Q1: "dog", Q2: "cat", Q3: "goat", Q4: "bird"}
+
+require_relative 'questionsAnswers.rb'
+array = [Q1, Q2, Q3, Q4]
+hash = {Q1 => [A1, A2], Q2 => [B1, B2], Q3 => [C1, C2], Q4 => [D1, D2]}
+
+#These are our variables
 i=0
 j=1
 
 while i < 4 do
-
     # Gives a random index from the array and stores it in variable position
     position = rand(array.length)
 
@@ -18,9 +21,9 @@ while i < 4 do
 
     puts random_question
     answer = gets.chomp.downcase
-    if answer == hash[random_question]
+    if answer == hash[random_question][0]
         system 'clear'
-        puts "Correct"
+        puts "Correct" + " #{A2}"
         sleep(2)
         system 'clear'
     else
