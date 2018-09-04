b453242f2395581f1f5bfc65094f57845ffab47e
David Ramsdale <DavidRamsdale1991@gmail.com>
2018-09-04 16:36:45 +1000

    update terminal-bias file

--- lib/terminalBiasTrainer.rb
+++ lib/terminalBiasTrainer.rb
@@ -6,7 +6,6 @@
 require_relative 'questionsAnswers.rb'
 #require_relative 'countdownTest.rb'
 require_relative 'user.rb'
-
 @array = [Q1, Q2, Q3, Q4]
 hash = {Q1 => [A1, A2], Q2 => [B1, B2], Q3 => [C1, C2], Q4 => [D1, D2]}
 
